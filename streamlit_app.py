4f4b00d056b8f4b9c88201038aa279199343a844
saibhavana2001 <vandanapusaibhavana@gmail.com>
2025-07-01 18:26:15 +0530

    added api data in the from of dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,8 +42,8 @@ if ingredients_list:
 
 import requests
 smoothiefruit_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefruit_response.json())
-sf_df = st.dataframe(data=smoothiefruit_response, use_container_width=True) 
+#st.text(smoothiefruit_response.json())
+sf_df = st.dataframe(data=smoothiefruit_response.json(), use_container_width=True) 
 
 cur.close()
 cnx.close()
